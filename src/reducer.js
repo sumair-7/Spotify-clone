10474377314788199b56288298b45e0e860f40a0
sumair-7 <shaiksumairpasha3030@gmail.com>
2022-04-06 00:43:05 +0530

    developing Spotify clone app

---
+++ src/reducer.js
@@ -0,0 +1,30 @@
+export const initialState = {
+    user: null,
+    playlists: [],
+    playing: false,
+    item: null,
+    token: 
+    "BQDwVMlNB3RYnxvTDReDzG0hdtkw-v6EdLN9DqWt2OCBsEpkBf44DdFRybWjTCEIuqW0qcpIpj4dYFuJ_d6nvs4Oc6Ni4QjtoQFKW8ZxpaOi8iJjaLavJTy3p_8g_Mb_KfKjRL8RMlXYdeGi0w9R_5958DtxQEIW96uwbRyWMJ-Dg7hr",
+};
+
+const reducer = (state, action) => {
+
+    console.log(action);
+// Action -> type, [payload]
+    switch(action.type){
+        case 'SET_USER':
+            return {
+                ...state, 
+                user: action.user,
+            };
+        case 'SET_TOKEN':
+            return {
+                ...state,
+                token: action.token,
+            };
+        default: 
+        return state;
+    }
+};
+
+export default reducer;
